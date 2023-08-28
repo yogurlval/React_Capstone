f9a661f2e47f14c50f10feb49ae717d34abca49c
Valentina Bowman <yogurlval@gmail.com>
2023-08-28 02:25:12 -0600

    styling and cart functionality

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,35 +1,84 @@
-import React from 'react';
-import { useCart } from '../components/CartContext';
+import React from "react";
+import { useCart } from "../components/CartContext";
+import "./Cart.css";
 
 const Cart = () => {
-  const { cartState, dispatch } = useCart(); // Get dispatch function from useCart
-  const { items } = cartState;
+  const { cartState, dispatch } = useCart();
+  let { items } = cartState;
 
-  const removeFromCart = (index) => {
-    dispatch({ type: 'REMOVE_FROM_CART', payload: index });
+  // Consolidate items with the same name and price into one entry
+  const consolidatedItems = [];
+  items.forEach((item) => {
+    const existingItem = consolidatedItems.find(
+      (consolidatedItem) =>
+        consolidatedItem.name === item.name &&
+        consolidatedItem.price === item.price
+    );
+    if (existingItem) {
+      existingItem.quantity += 1;
+    } else {
+      consolidatedItems.push({ ...item, quantity: 1 });
+    }
+  });
+
+  const calculateTotalPrice = (items) => {
+    return items.reduce(
+      (total, item) =>
+        total + parseFloat(item.price.replace("$", "")) * item.quantity,
+      0
+    );
   };
 
+  const removeFromCart = (name, price) => {
+    const itemIndex = items.findIndex(
+      (item) => item.name === name && item.price === price
+    );
+  
+    if (itemIndex !== -1) {
+      dispatch({ type: "REMOVE_FROM_CART", payload: itemIndex });
+    }
+  };
+
+  const total = calculateTotalPrice(consolidatedItems);
+
   return (
-    <div>
-      <h2>Your Cart</h2>
-      {items.length === 0 ? (
+    <div className="cartBody">
+    <div className="Cart-Container">
+      <h2 className="cartHeader">Your Cart</h2>
+      {consolidatedItems.length === 0 ? (
         <p>Your cart is empty.</p>
       ) : (
-        <ul>
-          {items.map((item, index) => (
-            <li key={index}>
-              <h4>{item.name}</h4>
-              {item.specialRequests && <p>Special Requests: {item.specialRequests}</p>}
-              {item.price}
-              <button onClick={() => removeFromCart(index)}>Remove</button>
+        <div className="cartList">
+          <ul>
+            <li className="cartCategories">
+              <h3>Name</h3>
+              <h3>Price</h3>
+              <h3>Quantity</h3>
+              <br />
             </li>
-          ))}
-        </ul>
+            {consolidatedItems.map((item) => (
+              <div className="cartListItems">
+                <li key={`${item.name}-${item.price}`}>
+                  <div>
+                  <h4>{item.name}</h4>
+                  {item.specialRequests && (
+                    <p>Special Requests: {item.specialRequests}</p>
+                  )}
+                  </div>
+                  <p className="price">{item.price}</p>
+                  {item.quantity > 1 ? <p>{item.quantity}</p> : <p>1</p>}
+                  <button className="removeBtn" onClick={() => removeFromCart(item.name, item.price)}>Remove</button>
+                </li>
+              </div>
+            ))}
+          </ul>
+          <p className="cartTotal">Total: ${total.toFixed(2)}</p>
+        </div>
       )}
+    </div>
     </div>
   );
 };
 
 export default Cart;
-
 
